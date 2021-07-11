e9aec40d3d352551837058134cb559738a709357
Matthew Smith <08msmith6@gmail.com>
2021-07-11 23:07:16 +0100

    commiting

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ python3 -m venv /opt/certbot/
 /opt/certbot/bin/pip install certbot
 ln -s /opt/certbot/bin/certbot /usr/bin/certbot
 certbot certonly -d example.com --debug-challenges --standalone --dry-run
-certbot certonly --standalone -d games.mattsmith.app -m 08msmith6@gmail.com --agree-tos -n --preferred-challenges tls-sni
+certbot certonly --standalone -d games.mattsmith.app -m 08msmith6@gmail.com --agree-tos -n --preferred-challenges tls-sni-01
 echo "0 0,12 * * * root /opt/certbot/bin/python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew -q" | tee -a /etc/crontab > /dev/null
 
 python manage.py migrate
